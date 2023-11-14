19cc1b905e23bab9fa7b355894c8bd62123fff91
Oliver Warlick <146263151+MrOliverW@users.noreply.github.com>
2023-11-14 10:36:39 -0800

    Add files via upload

--- JSON.py
+++ JSON.py
@@ -13,7 +13,7 @@ data = {
     'is_student': True
 }
 
-#
+#Provides instruction for how data should be writted to the json file
 with open('data.json','w') as json_file:
 
     json.dump(data,json_file,indent=4)
@@ -21,7 +21,7 @@ with open('data.json','w') as json_file:
 print('Data has been written to data.json')
 
 
-#
+#Provides instruction for how data should be read 
 with open('data.json','r') as json_file:
 
     loaded_data = json.load(json_file)
@@ -29,11 +29,11 @@ with open('data.json','r') as json_file:
 print("Successfully able to read data.json")
 print(loaded_data)
 
-#
+#Overwrites data variable and appends an item to the json file
 loaded_data['age'] = 34
 loaded_data['interests'].append('Mini Golfing')
 
-#
+#Provides instruction for how data should be overwritten on the json file
 with open('data.json', 'w') as json_file:
 
     json.dump(loaded_data, json_file, indent=4)
